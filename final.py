d70b46b3a8e130cd0670762722bb67643f4e24ee
code49 <davidlechan@gmail.com>
2021-06-22 13:15:38 -0700

    added clear() and inline() functions

--- final.py
+++ final.py
@@ -2,10 +2,28 @@
 
 """
 This is just to add certain print functions I want to use in programs - final print (just to show it's meant to be in the final version of the program), 
-clear (clear terminal window), inlineA().
+clear (clear terminal window), inline().
 """
+
+#----- import stuff -----
+
+import os
+import sys
 
 #----- code -----
 
 def finalPrint(content):
+    print(content)
+
+def clear():
+    #for windows 
+    if os.name == 'nt': 
+        _ = os.system('cls') 
+  
+    #for mac and linux (os.name is 'posix') 
+    else: 
+        _ = os.system('clear') 
+
+def inline(content):
+    sys.stdout.write("\033[2K\033[1G")
     print(content)
