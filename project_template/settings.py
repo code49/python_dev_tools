916ecb1d3f3dd041cb1493415dd124df09b04f65
code49 <davidlechan@gmail.com>
2021-07-05 17:09:01 -0500

    Changed settings.py to use a provided list

--- project_template/settings.py
+++ project_template/settings.py
@@ -1,13 +1,14 @@
 #----- intro -----
 
 """
-This file is meant to streamline importing environment variables into projects.
+<add introductory comments here> 
 """
 
 #----- load environment variables -----
 
 #setup dotenv properly
 from dotenv import load_dotenv, find_dotenv
+from pathlib import Path
 import os
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
@@ -16,10 +17,25 @@ load_dotenv(find_dotenv())
 
 #----- create function to return environment variables to main file -----
 
-def getSettings():
-    settings = {
-    #for each environment variable, use os.getenv() to load the value its key
+def getSettings(settings_list):
+    """
+    Function that extracts settings from a .env file
+
+    Parameters
+    ----------
     
-    }
+    settings_list: list
+        list of strings containing each setting's name
+
+    Returns
+    -------
+
+    settings: dict
+        dictionary of settings
+
+    """
+    settings = {}
+    for setting in settings_list:
+        settings[setting] = os.getenv(setting)
     
     return settings
