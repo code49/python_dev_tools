df040c015f222c12529038527161086c6277e3b0
NUS12679-12-davchan <davchan@nus12679-12-davchan.nueva.local>
2021-10-06 09:26:31 -0700

    Added custom horizontal rule functionality through dashedFormattedLine()

--- final.py
+++ final.py
@@ -9,6 +9,7 @@ clear (clear terminal window), inline().
 
 import os
 import sys
+from custom_errors import errorCreator
 
 #----- code -----
 
@@ -18,6 +19,7 @@ class finalCommands():
         
         #only thing that needs to be set up is an internal ticker for whether dev mode is enabled or not - this allows for certain functions that only occur in the final version and not while in dev mode
         self.dev_mode = dev_mode
+        self.errorCreator = errorCreator()
 
     #basic printing; mostly for clarity reasons
     def finalPrint(self, content):
@@ -41,6 +43,22 @@ class finalCommands():
     #printing blank lines
     def blankLine(self):
         print()
+
+    #printing a line that's either the length of the terminal or the length of the longest string in a set of strings
+    def dashedFormattedLine(self, string_list):
+        #sanity check to ensure that we have received a list of length >= 1
+        if type(string_list) != "list" or len(string_list) == 0:
+            self.errorCreator.printError("TypeError", "finalCommands:dashedFormmatedLine()", "expected a list of at least length 1, either didn't get a list or got an empty one", "something has gone wrong, please inform the creator of the program", "", exit=True)
+        
+        #determine if the maximum length of a string in string_list is less than the width of the terminal and print the dashed line accordingly
+        max_string_length = max(string_list)
+        terminal_width = os.get_terminal_size
+        line_width = max(max_string_length, terminal_width)
+        temp_line = ""
+        for i in range(0, line_width):
+            temp_line.append("-")
+        print(temp_line)
+        
 
     #clearing the terminal window, but ensuring that dev mode is disabled in order to do so - see finalNoDevPrint()
     def noDevClear(self):
