9fabf05eb663053f43f3941786b56e875cbf1827
code49 <davidlechan@gmail.com>
2022-04-23 02:09:16 -0700

    updated project template main file

--- project_template/main.py
+++ project_template/main.py
@@ -4,29 +4,31 @@
 <add introductory comments here> 
 """
 
-# ----- setup dev stuff -----
+# ----- import required libraries -----
 
-# module setup
+import time
+
+# ----- messager setup -----
+
+from pytools import module_setup  # import the setup routine to be run
 import settings
-from python_dev_tools.final import finalCommands
-from python_dev_tools.dev import devCommands
-from python_dev_tools import module_setup  # import the setup routine to be run
+from pytools.messager import messagerSetup, clear, horizontalRule
+logger = messagerSetup()
+
+# ----- module setup -----
+
 # add in strings of require-install modules here
 module_list = ["python-dotenv"]
 module_setup.main(module_list)
-
-# classes for functions relating to printing things
-
-dev_mode = True
-dev_commands_object = devCommands(dev_mode)
-
-final_commands_object = finalCommands(dev_mode)
 
 # ----- load environment variables -----
 
 settings = settings.getSettings()
 
-# ----- import required modules -----
+# ----- completion message -----
+
+time.sleep(1)
+logger.info("setup complete.")
 
 # ----- code -----
 
